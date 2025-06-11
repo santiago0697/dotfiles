055a0b76c3a5994f5441cdbf9e150698329d1cff
Santiago Correa Muñoz <scorrea153@gmail.com>
2025-06-11 11:11:15 +0200

    .

--- .config/nvim/lua/colorscheme.lua
+++
@@ -1,2 +0,0 @@
-vim.cmd('colorscheme gruvbox')
-vim.cmd('let g:nvcode_termcolors=256')
--- .config/nvim/lua/nv-barbar.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd([[
-  let bufferline = get(g:, 'bufferline', {})
-  let bufferline.closable = v:false
-  let bufferline.clickable = v:false
-  let bufferline.animation = v:false
-]])
